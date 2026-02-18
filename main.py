ed999d33b7929907b902e1278360556e3401abf5
Nathaniel Tiong <tiongson.nathan28@gmail.com>
2026-02-18 20:30:50 +0800

    Add comments to sign_up function for clarity
    Added comments to clarify the sign-up function's logic and purpose.

--- main.py
+++ main.py
@@ -3,13 +3,19 @@ from pyscript import document
 def sign_up(e):
     username = document.getElementById("username_input").value
     password = document.getElementById("password_input").value
+    #these codes gets the input from the html
     result = document.getElementById("result1")
+    #this is for the result
 
     if username == "admin" and password == "676767":
         result.innerHTML = "<b>Success</b>"
+    #this if statement makes sure that the username is = admin and the password is 676767
     elif password != "676767":
         result.innerHTML = "Invalid password"
     else:
         result.innerHTML = "Wrong username"
+
+    #if the password and or username isnt equal to admin or 676767 it shows invalid password or wrong username
+
 
 
